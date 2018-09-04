7b8e746250f5c2b072631a4e2846dde1e89e0d72
Niket Naidu <coder137@gmail.com>
2018-09-04 15:52:57 +0530

    Updated mongodbrunner.bat

--- python/LocalTests/windows_start_mongodbrunner.bat
+++ python/LocalTests/windows_start_mongodbrunner.bat
@@ -1,6 +1,9 @@
+@echo off
+echo "Make sure you run this as ADMINISTRATOR"
 echo "Setting MONGODB_PURGE=false"
 set MONGODB_PURGE=false
 
 echo "Starting mongodb-runner"
 mongodb-runner start
 echo "Started mongodb-runner"
+pause
