a28778b4c0dbbf1bfce9f48655be0fcc60d8b267
Niket Naidu <coder137@gmail.com>
2018-09-04 15:44:49 +0530

    Revert "Updated LocalTest and mongodb_runner.bat"
    This reverts commit 305f1eaf65e3c85ac0aff8fdc5845664a21866a5.

--- python/LocalTests/postRequestTest/httpconstants.py
+++
@@ -1,6 +0,0 @@
-x_parse_application_id = "X-Parse-Application-Id"
-x_parse_rest_api_key = "X-Parse-REST-API-Key"
-content_type = "Content-Type"
-
-## Different content types
-application_json = "application/json"
--- python/LocalTests/windows_start_mongodbrunner.bat
+++ python/LocalTests/windows_start_mongodbrunner.bat
@@ -1,8 +1,6 @@
-@echo off
 echo "Setting MONGODB_PURGE=false"
 set MONGODB_PURGE=false
 
 echo "Starting mongodb-runner"
 mongodb-runner start
 echo "Started mongodb-runner"
-pause
